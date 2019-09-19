5bca6842e9a7970c01940991765d17dc330168cc
Josh <asuraengine@hotmail.com>
2019-09-19 17:49:16 +0800

    666999

---
+++ Studio 1/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public void StartPress()
+    {
+
+    }
+
+    public void PauseGame()
+    {
+
+    }
+
+
+       
+}
