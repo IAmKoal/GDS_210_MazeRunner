522fe702d4713eacbd945fd82324c7b08fbdf68c
Josh Shinnick <asuraengine@hotmail.com>
2019-09-26 15:13:52 +0800

    Auto stash before revert of "Merge branch 'JJ' into WorkingMaps"

---
+++ Studio 1/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    Scene mainGame, pauseGame;
+    public void StartPress()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void PauseGame()
+    {
+
+    }
+
+    //by Josh Shinnick.
+}
